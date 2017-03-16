6380c16950268ce836eb4dc458fb49e46fae3cc5
lixiaodongdg <lxd@qbl.local>
2017-03-16 15:39:57 +0800

    Initial Commit

---
+++ ScanTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScanTest
+//
+//  Created by lxd on 2017/3/16.
+//  Copyright © 2017年 888888. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
